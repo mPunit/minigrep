e7141a2b6ba1376e3c20332aaa68b427b0562237
Punit <punitramyadav@gmail.com>
2024-10-04 20:43:13 -0400

    Modifying main.rs to handle errors and print it on standard error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+//use env_logger;
+//use log::{debug, info};
+use std::env;
+use std::process;
+
+use minigrep::{run, Config};
+
 fn main() {
-    println!("Hello, world!");
+    //env_logger::init();
+
+    let args: Vec<String> = env::args().collect();
+
+    println!("{:?}", args);
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    };
 }
